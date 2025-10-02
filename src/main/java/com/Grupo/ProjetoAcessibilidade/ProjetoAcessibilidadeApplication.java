b18193f6d1d18be2f184d28d1f6a87d1401ab3a4
Samin Ghoddosi <saminghoddosi44@gmail.com>
2025-10-02 11:53:46 -0300

    First Commit

---
+++ src/main/java/com/Grupo/ProjetoAcessibilidade/ProjetoAcessibilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.Grupo.ProjetoAcessibilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAcessibilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAcessibilidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Grupo/ProjetoAcessibilidade/ProjetoAcessibilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo.ProjetoAcessibilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcessibilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
